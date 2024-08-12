3bb360e9ce1dbeefa4b679ff055fcfdfec709ca5
Emmaokogho <157544958+Emmaokogho@users.noreply.github.com>
2024-08-12 01:04:34 -0400

    cache elements

---
+++ app.js
@@ -0,0 +1,7 @@
+// Cache elements
+const form = document.querySelector('#quiz-form');
+const questionsContainer = document.querySelector('#questions-container');
+const resultDiv = document.querySelector('#result');
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+const submitBtn = document.querySelector('#submit-btn');
